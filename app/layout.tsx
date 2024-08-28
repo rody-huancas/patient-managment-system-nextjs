b0a9e8a4d386f42bef0eca357dea5ef20ddcdc4f
Rody Huancas <rodyhuancas.04@gmail.com>
2024-08-27 20:38:23 -0500

    Feat: utils, styles and config for tailwind

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,27 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Plus_Jakarta_Sans } from "next/font/google";
+
+import { cn } from "@/lib/utils";
 
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const fontSans = Plus_Jakarta_Sans({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-sans",
+});
 
 export const metadata: Metadata = {
   title: "CarePulse",
   description: "A healthcare management system",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={cn("min-h-screen bg-dark-300 font-sans antialiased", fontSans.variable)}>
+        {children}
+      </body>
     </html>
   );
 }
