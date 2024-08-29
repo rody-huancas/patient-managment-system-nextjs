5e13173bb62c56dfa9923abebe08d30d27ea23b7
Rody Huancas <rodyhuancas.04@gmail.com>
2024-08-28 20:30:52 -0500

    Feat: home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,7 @@
+import Link from "next/link";
 import Image from "next/image";
+
+import PatientForm from "@/components/forms/PatientForm";
 
 export default function Home() {
   return (
@@ -12,8 +15,27 @@ export default function Home() {
             alt="patient"
             className="mb-12 h-10 w-fit"
           />
+
+          <PatientForm />
+
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              © 2024 CarePulse
+            </p>
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
+            </Link>
+          </div>
         </div>
       </section>
+
+      <Image
+        src="/assets/images/onboarding-img.png"
+        height={1000}
+        width={1000}
+        alt="patient"
+        className="side-img max-w-[50%]"
+      />
     </div>
   );
 }
